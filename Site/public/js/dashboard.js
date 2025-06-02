bf37e370dd2da9c958c5c10ae75d9a1cfcfb590a
jpPinheiro03 <joao.pinheiro@sptech.school>
2025-06-01 21:40:17 -0300

    falta apenas o painel

--- Site/public/js/dashboard.js
+++ Site/public/js/dashboard.js
@@ -1,88 +1,99 @@
 function listar() {
-console.log(sessionStorage.ID_USER);
-let idUsuario = sessionStorage.ID_USER;
+    console.log(sessionStorage.ID_USER);
+    let idUsuario = sessionStorage.ID_USER;
 
     fetch(`/quiz/listar/${idUsuario}`)
-    .then(function (resposta) {
-        console.log("resposta: ", resposta);
+        .then(function (resposta) {
+            console.log("resposta: ", resposta);
 
-        if (resposta.ok) {
-            resposta.json().then(function (resposta) {
-                obterDadosGrafico(idUsuario)
+            if (resposta.ok) {
+                resposta.json().then(function (resposta) {
+                    obterDadosGrafico(idUsuario)
 
-            document.getElementById("graficos").innerHTML += `
-                <div id="grafico${idUsuario}" class="display-none">
-                    <h3 class="tituloGraficos">
-                        <span id="tituloAquario${idUsuario}">${resposta[0].nome} o restultado do seu quiz foi:</span>
-                        </h3>
-                        <h3><span id="tituloAquario${idUsuario}">Acertos: ${resposta[0].acertos}</span></h3>
-                        <h3><span id="tituloAquario${idUsuario}">Erros :${resposta[0].erradas}</span></h3>
-                        <h3><span id="tituloAquario${idUsuario}">Resultado : ${resposta[0].acertos * 10} pontos</span></h3>
-                    <div class="graph">
-                        <canvas id="myChartCanvas${idUsuario}"></canvas>
-                    </div>
-                    <h2>Você pode tentar denovo para obter novos resultados!</h2>
-                    <div class="label-captura">
-                        <p id="avisoCaptura${idUsuario}" style="color: white"></p>
-                    </div>
-                </div>
-            `
+                    document.getElementById("graficos").innerHTML += `
+    <div id="grafico${idUsuario}" class="display-none">
+        <h3 class="tituloGraficos">
+            <span>${resposta[0].nome}, o resultado do seu quiz foi:</span>
+        </h3>
+        <div class="kpi-container">
+            <div class="kpi">
+                <h3>Acertos</h3>
+                <span>${resposta[0].acertos}</span>
+            </div>
+            <div class="kpi">
+                <h3>Erros</h3>
+                <span>${resposta[0].erradas}</span>
+            </div>
+            <div class="kpi">
+                <h3>Resultado</h3>
+                <span>${resposta[0].acertos * 10} pts</span>
+            </div>
+        </div>
+        <div class="graph">
+            <canvas id="myChartCanvas${idUsuario}"></canvas>
+        </div>
+        <div class="retry-card">
+    <h2>Não gostou do seu resultado?</h2>
+    <a href="../pages/quiz.html">Tentar novamente</a>
+</div>
+    </div>
+`
 
                 });
-            
-        } else {
-            alert("Houve um erro ao tentar puxar os dados!");
-        }
-    })
-    .catch(function (erro) {
-        console.error("#ERRO: ", erro);
-        alert("Erro ao comunicar com o servidor.");
-    });
+
+            } else {
+                alert("Houve um erro ao tentar puxar os dados!");
+            }
+        })
+        .catch(function (erro) {
+            console.error("#ERRO: ", erro);
+            alert("Erro ao comunicar com o servidor.");
+        });
 
     return false;
 }
 
- 
+
 
 
 let graficoExibir = document.getElementById(`grafico${idUsuario}`)
-        graficoExibir.classList.remove("display-none")
-        graficoExibir.classList.add("display-block")
+graficoExibir.classList.remove("display-none")
+graficoExibir.classList.add("display-block")
 
-        let btnExibir = document.getElementById(`btnAquario${idUsuario}`)
-        btnExibir.classList.remove("btn-white")
-        btnExibir.classList.add("btn-pink")
+let btnExibir = document.getElementById(`btnAquario${idUsuario}`)
+btnExibir.classList.remove("btn-white")
+btnExibir.classList.add("btn-pink")
 
 
 
 
 function obterDadosGrafico(idUsuario) {
 
-        fetch(`/quiz/listar/${idUsuario}`, { cache: 'no-store' }).then(function (response) {
-            if (response.ok) {
-                response.json().then(function (resposta) {
-                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-                    resposta.reverse();
+    fetch(`/quiz/listar/${idUsuario}`, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                resposta.reverse();
 
-                    plotarGrafico(resposta, idUsuario);
+                plotarGrafico(resposta, idUsuario);
 
-                });
-            } else {
-                console.error('Nenhum dado encontrado ou erro na API');
-            }
-        })
-            .catch(function (error) {
-                console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
             });
-    }
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+}
 
 
-    function plotarGrafico(resposta, idUsuario) {
-        console.log('iniciando plotagem do gráfico...');
+function plotarGrafico(resposta, idUsuario) {
+    console.log('iniciando plotagem do gráfico...');
 
-        let labels = [];
+    let labels = [];
 
-         const dados = {
+    const dados = {
         labels: [
             'erradas',
             'certas',
@@ -99,35 +110,35 @@ function obterDadosGrafico(idUsuario) {
 
     };
 
-        console.log('----------------------------------------------')
-        console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
-        console.log(resposta)
+    console.log('----------------------------------------------')
+    console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
+    console.log(resposta)
 
-            labels.push(resposta[0].acertos);
-            dados.datasets[0].data.push(resposta[0].acertos);
-
-        
-
-        console.log('----------------------------------------------')
-        console.log('O gráfico será plotado com os respectivos valores:')
-        console.log('Labels:')
-        console.log(labels)
-        console.log('Dados:')
-        console.log(dados.datasets)
-        console.log('----------------------------------------------')
+    labels.push(resposta[0].acertos);
+    dados.datasets[0].data.push(resposta[0].acertos);
 
 
-        const config = {
-            type: 'pie',
-            data: dados,
-        };
 
-    
-            new Chart(
-            document.getElementById(`myChartCanvas${idUsuario}`),
-            config
-        );
-    }
+    console.log('----------------------------------------------')
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
+
+    const config = {
+        type: 'pie',
+        data: dados,
+    };
+
+
+    new Chart(
+        document.getElementById(`myChartCanvas${idUsuario}`),
+        config
+    );
+}
 
 
 
@@ -143,7 +154,7 @@ function listarEmpunhadura() {
                     document.getElementById("textinho").innerHTML += `
                      <h3><span> Exibindo quantidade de  empunhaduras por usúarios  </span></h3>
                     `
-                    
+
                 });
             } else {
                 alert("Houve um erro ao tentar puxar os dados!");
@@ -194,13 +205,13 @@ function plotarGraficoEmpunhadura(dados) {
     };
 
     console.log('----------------------------------------------')
-        console.log('O gráfico será plotado com os respectivos valores:')
-        console.log('Labels:')
-        console.log(labels)
-        console.log('Dados:')
-        console.log(dados.datasets)
-        console.log('----------------------------------------------')
-  
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
     document.getElementById("graficoEmpunhadura").classList.remove("display-none");
     document.getElementById("graficoEmpunhadura").classList.add("display-block");
 
